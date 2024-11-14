ef52d5cb249b189484c0879ac93182ff67f50f48
ShiviiSonii <1111dhruvsoni@gmail.com>
2024-11-14 22:26:12 +0530

    add contact model

---
+++ backend/src/models/contact.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const contactSchema = new mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone_number: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    job_title: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Contact = mongoose.model("Contact", contactSchema);
