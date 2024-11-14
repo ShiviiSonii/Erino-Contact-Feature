42f47c1a45209e1139afcf727008f685d6e5175f
ShiviiSonii <1111dhruvsoni@gmail.com>
2024-11-14 22:14:23 +0530

    mongodb connected

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./src/db/index.js";
 
 const app = express();
-dotenv.config();
-
-app.get("/", (req, res) => {
-  console.log("Welcome to Erino Contact Feature");
+dotenv.config({
+  path: "./.env",
 });
 
-const port = process.env.PORT | 8000;
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Erino Contact Feature");
+});
+
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log("Server is listening on port:", port);
